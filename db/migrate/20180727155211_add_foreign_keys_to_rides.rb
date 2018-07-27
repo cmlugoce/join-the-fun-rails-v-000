1618aed9c256178fb66938a24b033ca3aa823dd2
Cristina Lugo <cmlugoce@syr.edu>
2018-07-27 15:52:55 +0000

    automatically backed up by learn

--- db/migrate/20180727155211_add_foreign_keys_to_rides.rb
+++ db/migrate/20180727155211_add_foreign_keys_to_rides.rb
@@ -1,7 +1,7 @@
 class AddForeignKeysToRides < ActiveRecord::Migration
   def change
-    add_column :rides, :add_column, :string
-    add_column :rides, :, :passenger_id
+
+    add_column :rides, :passenger_id, :integer
     add_column :rides, :taxi_id, :integer
   end
 end
